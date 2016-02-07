5408a693057a100b3bbb18ccf7d57f29185e6f29
Jonathan Smith <jnthsmth@umich.edu>
2016-02-07 17:45:55 -0500

    GravNav is dead, long live GravNav
    Resurrected from the fubar repo

---
+++ Assets/Scripts/PlanetScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetScript : MonoBehaviour {
+
+    public float strengthOfAttraction = 0;
+	
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerScript : MonoBehaviour {
+
+    public static GameObject S;
+	private Rigidbody rigid;
+	public Vector3 initialVelocity;
+	public PlanetScript[] attractors;
+	public Vector3 lastAppliedForce;
+
+	void Start() {
+		rigid = GetComponent<Rigidbody>();
+		if (rigid == null) print("Error! No Rigidbody component on Player");
+		rigid.velocity = initialVelocity;
+	}
+	
+	void Awake() {
+		if (S) {
+			Debug.Log("Multiple players. Error.");
+			return;
+		}
+		S = gameObject;
+	}
+	
+	void FixedUpdate() {
+		foreach (PlanetScript ps in attractors) {
+			lastAppliedForce = gravity(ps);
+			rigid.AddForce(lastAppliedForce);
+		}
+	}
+	
+	Vector3 gravity(PlanetScript ps) {
+		Vector3 displacement = ps.gameObject.transform.position - transform.position;
+		return displacement.normalized * ps.strengthOfAttraction / Mathf.Pow(displacement.magnitude, 2);
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawLine(transform.position, transform.position + 4 * lastAppliedForce);
+	}
+}

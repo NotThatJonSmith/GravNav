654adf9573edc47ed20c4c6d160df632b7e30d65
Jonathan Smith <jnthsmth@umich.edu>
2016-02-08 18:52:52 -0500

    Drawing trajectory added

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -25,10 +25,14 @@ public class PlayerScript : MonoBehaviour {
 		// TODO populate the array with ALL attractors in scene?
 	}
 	
+	void Update() {
+		drawTrajectory();
+	}
+
 	void FixedUpdate() {
 		foreach (PlanetScript ps in attractors) {
 			if (ps.disableForce) continue;
-			lastAppliedForce = gravity(ps);
+			lastAppliedForce = gravity(ps, transform.position);
 			rigid.AddForce(lastAppliedForce);
 		}
 		if (rigid.velocity.magnitude != 0f) {
@@ -37,22 +41,38 @@ public class PlayerScript : MonoBehaviour {
 		
     }
 	
-	Vector3 gravity(PlanetScript ps) {
-		Vector3 displacement = ps.gameObject.transform.position - transform.position;
-		return displacement.normalized * ps.strengthOfAttraction / Mathf.Pow(displacement.magnitude, 2);
+	Vector3 gravity(PlanetScript ps, Vector3 pos) {
+		Vector3 displacement = ps.gameObject.transform.position - pos;
+		return displacement.normalized * ps.strengthOfAttraction / displacement.magnitude;
 	}
 
 	void OnDrawGizmos() {
 		Gizmos.DrawLine(transform.position, transform.position + 4 * lastAppliedForce);
 	}
 
-	public float forecastTime = 3.0f;
+	public float forecastTime = 100.0f;
+	public float deadZoneRadius = 2f;
 	void drawTrajectory() {
-		Vector3 cumulativeForce = Vector3.zero;
-		foreach (PlanetScript ps in attractors) {
-			if (ps.disableForce) continue;
-			lastAppliedForce = gravity(ps);
-			rigid.AddForce(lastAppliedForce);
+		int trajectorySteps = (int)(forecastTime / Time.timeScale);
+		Vector3 pos = transform.position;
+		Vector3 vel = rigid.velocity;
+		for (int i = 0; i < trajectorySteps; i++) {
+			Vector3 cumulativeForce = Vector3.zero;
+			foreach (PlanetScript ps in attractors) {
+				if (ps.disableForce) continue;
+				cumulativeForce += gravity(ps, pos);
+			}
+			Vector3 nextPos = pos;
+			vel += cumulativeForce / rigid.mass / Time.timeScale;
+			nextPos += vel / Time.timeScale;
+			foreach (PlanetScript ps in attractors) {
+				if (ps.disableForce) continue;
+				if ((ps.gameObject.transform.position - nextPos).magnitude < deadZoneRadius) return;
+			}
+			Debug.DrawLine(pos, nextPos, Color.red);
+			pos = nextPos;
+
 		}
+		
 	}
 }

3d9a2b073be38956c227e7d191fedffc0d414d68
Jonathan Smith <jnthsmth@umich.edu>
2016-02-08 16:59:21 -0500

    Clicking controls planet gravity now.

--- Assets/Scripts/PlanetScript.cs
+++ Assets/Scripts/PlanetScript.cs
@@ -2,7 +2,41 @@
 using System.Collections;
 
 public class PlanetScript : MonoBehaviour {
-
-    public float strengthOfAttraction = 0;
 	
+	public float strengthOfAttraction = 0;
+	public bool _disableForce = false;
+	public Material[] mats;
+	public Renderer rend;
+
+	void Awake() {
+		rend = GetComponent<Renderer>();
+	}
+
+	void setMaterial() {
+		rend.sharedMaterial = mats[disableForce?0:1];
+	}
+
+	void Start() {
+		disableForce = true;
+		setMaterial();
+	}
+
+	public bool disableForce {
+		get {
+			return _disableForce;
+		}
+
+		set {
+			_disableForce = value;
+			setMaterial();
+		}
+	}
+
+	void OnMouseDown() {
+		disableForce = !disableForce;
+	}
+
+
+
+
 }

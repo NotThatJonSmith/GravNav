c6b07bf87ff2aa7a85728b996f3de17fa9cce07c
anashif <anashif@umich.edu>
2016-02-08 18:55:15 -0500

    Changed thrusters

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -2,38 +2,42 @@
 using System.Collections;
 
 public class Thruster : MonoBehaviour {
-    //The forward direction of the ship
-    //Thrust is applied opposite this
-    public Vector3 forwardVector;
-    //Multiplies the forwardVector to determine the force
+    public Vector3  upVector;
+    public int      upUses;
+    public Vector3  downVector;
+    public int      downUses;
+    public Vector3  leftVector;
+    public int      leftUses;
+    public Vector3  rightVector;
+    public int      rightUses;
     public float thrust;
-    //The starting fuel in seconds
-    public float initialFuel;
-    //How much fuel is left
-    //This does not need to be set in the Inspector
-    public float remainingFuel;
-    //If true, the force is being applied
-    public bool active;
 
     // Use this for initialization
     void Start() {
-        remainingFuel = initialFuel;
-        active = false;
+
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey("space") && remainingFuel > 0) {
-            active = true;
-            remainingFuel -= Time.deltaTime;
-        } else {
-            active = false;
+        if (Input.GetKeyDown(KeyCode.UpArrow) && upUses > 0) {
+            upUses--;
+            this.gameObject.GetComponent<Rigidbody>().AddRelativeForce(upVector * thrust, ForceMode.Force);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && downUses > 0) {
+            downUses--;
+            this.gameObject.GetComponent<Rigidbody>().AddRelativeForce(downVector * thrust, ForceMode.Force);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && leftUses > 0) {
+            leftUses--;
+            this.gameObject.GetComponent<Rigidbody>().AddRelativeForce(leftVector * thrust, ForceMode.Force);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && rightUses > 0) {
+            rightUses--;
+            this.gameObject.GetComponent<Rigidbody>().AddRelativeForce(rightVector * thrust, ForceMode.Force);
         }
     }
 
     void FixedUpdate() {
-        if (active) {
-            this.gameObject.GetComponent<Rigidbody>().AddRelativeForce(forwardVector * thrust * -1, ForceMode.Impulse);
-        }
+
     }
 }

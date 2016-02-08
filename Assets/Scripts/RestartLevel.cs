2ebf9f473116572b54f710c247a73cb1fcd08343
anashif <anashif@umich.edu>
2016-02-08 17:19:35 -0500

    Added restart level message

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class RestartLevel : MonoBehaviour {
+    public KeyCode resetKey;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(resetKey)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}

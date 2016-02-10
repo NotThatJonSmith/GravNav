63d2b6aa2002002e5177b420b36f79dbcc3ede79
anashif <anashif@umich.edu>
2016-02-10 14:28:23 -0500

    Added teleporters

---
+++ Assets/Scripts/TeleportObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportObject : MonoBehaviour {
+
+    public GameObject teleportTo;
+    public bool incoming;
+
+    private TeleportMaster tpM;
+
+	// Use this for initialization
+	void Start () {
+        tpM = this.gameObject.transform.parent.GetComponent<TeleportMaster>();
+        incoming = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.gameObject.GetComponent<PlayerScript>() && !tpM.teleported) {
+            coll.gameObject.transform.position = teleportTo.transform.position;
+            teleportTo.GetComponent<TeleportObject>().incoming = true;
+            tpM.teleported = true;
+        }
+    }
+
+    void OnTriggerExit(Collider coll) {
+        if (coll.gameObject.GetComponent<PlayerScript>() && incoming) {
+            incoming = false;
+            tpM.teleported = false;
+        }
+    }
+}
